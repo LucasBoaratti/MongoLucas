39b37284c2795c2380274a9a2038ad84c24225be
Lucas Henrique Boaratti Silva <47123916859@>
2025-09-03 11:35:30 -0300

    Adicionando comentários.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,20 @@
 import { Routes } from '@angular/router';
 import { Receitas } from './receitas/receitas';
-import { App } from './app';
 import { Culturas } from './culturas/culturas';
+import { Home } from './home/home';
 
 export const routes: Routes = [
+    // Rota inicial da página home
     {
         path: "",
+        component: Home
+    },
+    // Rota para a página de culturas
+    {
+        path: "culturas",
         component: Culturas
     },
+    // Rota para a página de receitas
     { 
         path: "receitas", 
         component: Receitas 
--- src/app/app.ts
+++ src/app/app.ts
@@ -3,6 +3,8 @@ import { RouterOutlet, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  // Standalone permite criar componentes, sem a necessidade de declarar módulos em NgModules
+  standalone: true,
   imports: [RouterOutlet, RouterModule],
   templateUrl: './app.html',
   styleUrls: ['./app.scss']
