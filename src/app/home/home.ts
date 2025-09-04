09427de6d6a61786b5d57ae455eced08c1560d7c
Lucas Boaratti <lucasboaratti15@gmail.com>
2025-09-04 16:38:58 -0300

    Página home finalizada.

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrl: './home.scss'
 })
 export class Home {
+    // Construtor que permite navegar entre rotas, sem precisar do routerLink
+    constructor(private router: Router) {}
 
+    // Função que muda a rota atual para a página de culturas
+    cultura(): void {
+        this.router.navigate(['/culturas']);
+    }
+    
+    // Navegação para a página de receitas
+    receita(): void {
+        this.router.navigate(["/receitas"]);
+    }
 }
