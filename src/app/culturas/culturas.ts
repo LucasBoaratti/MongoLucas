e094efc74d6d46ef8ca123c1e179b71b8244bdea
Lucas Henrique Boaratti Silva <47123916859@>
2025-09-05 11:32:22 -0300

    Adicionando o modal de trajes e novos nomes de variáveis editados.

--- src/app/culturas/culturas.ts
+++ src/app/culturas/culturas.ts
@@ -1,23 +1,36 @@
 import { Component } from '@angular/core';
-import { Culturamodal1 } from '../modais/festival/festival';
+import { Festival } from '../modais/festival/festival';
+import { Trajes } from "../modais/trajes/trajes";
 
 @Component({
   selector: 'app-culturas',
-  imports: [Culturamodal1],
+  imports: [Festival, Trajes],
   templateUrl: './culturas.html',
   styleUrl: './culturas.scss'
 })
 export class Culturas {
-    // Definindo o estado do modal como false (define se ele está aberto ou não)
-    openModal = false;
+    // Definindo o estado que controla se modal está visível (define se ele está aberto ou não)
+    openModalFestivais = false;
 
     // Função que abre o modal
-    abrirModal = () => {
-        this.openModal = true;
+    abrirModalFestivais = () => {
+        this.openModalFestivais = true;
     }
 
     // Função que fecha o modal
-    fecharModal = () => {
-        this.openModal = false;
+    fecharModalFestivais = () => {
+        this.openModalFestivais = false;
+    }
+
+    // Modal dos trajes nacionais
+
+    openModalTrajes = false;
+
+    abrirModalTrajes = () => {
+        this.openModalTrajes = true;
+    }
+
+    fecharModalTrajes = () => {
+        this.openModalTrajes = false;
     }
 }
