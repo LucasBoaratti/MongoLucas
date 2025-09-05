b145520c9f3f6dffbf96b8ff6b4af2e765a6cdd9
Lucas Boaratti <lucasboaratti15@gmail.com>
2025-09-05 01:09:03 -0300

    Modal de festival adicionado.

--- src/app/culturas/culturas.ts
+++ src/app/culturas/culturas.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { Culturamodal1 } from '../modais/festival/festival';
 
 @Component({
   selector: 'app-culturas',
-  imports: [],
+  imports: [Culturamodal1],
   templateUrl: './culturas.html',
   styleUrl: './culturas.scss'
 })
 export class Culturas {
+    // Definindo o estado do modal como false (define se ele está aberto ou não)
+    openModal = false;
 
+    // Função que abre o modal
+    abrirModal = () => {
+        this.openModal = true;
+    }
+
+    // Função que fecha o modal
+    fecharModal = () => {
+        this.openModal = false;
+    }
 }
