a96facc65b68aae75e65d927617423082fbd0335
Lucas Boaratti <lucasboaratti15@gmail.com>
2025-09-05 23:44:18 -0300

    Modais finalizados e estilizações alteradas.

--- src/app/culturas/culturas.ts
+++ src/app/culturas/culturas.ts
@@ -1,16 +1,18 @@
 import { Component } from '@angular/core';
 import { Festival } from '../modais/festival/festival';
 import { Trajes } from "../modais/trajes/trajes";
+import { Musicas } from '../modais/musicas/musicas';
+import { Dancas } from "../modais/dancas/dancas";
 
 @Component({
   selector: 'app-culturas',
-  imports: [Festival, Trajes],
+  imports: [Festival, Trajes, Musicas, Dancas],
   templateUrl: './culturas.html',
   styleUrl: './culturas.scss'
 })
 export class Culturas {
     // Definindo o estado que controla se modal está visível (define se ele está aberto ou não)
-    openModalFestivais = false;
+    openModalFestivais: boolean = false;
 
     // Função que abre o modal
     abrirModalFestivais = () => {
@@ -22,9 +24,9 @@ export class Culturas {
         this.openModalFestivais = false;
     }
 
-    // Modal dos trajes nacionais
+    // Controlando o modal dos trajes nacionais
 
-    openModalTrajes = false;
+    openModalTrajes: boolean = false;
 
     abrirModalTrajes = () => {
         this.openModalTrajes = true;
@@ -32,5 +34,29 @@ export class Culturas {
 
     fecharModalTrajes = () => {
         this.openModalTrajes = false;
+    }
+
+    // Controlando o estado do modal de músicas
+
+    openModalMusicas: boolean = false;
+
+    abrirModalMusicas = () => {
+        this.openModalMusicas = true;
+    }
+
+    fecharModalMusicas = () => {
+        this.openModalMusicas = false;
+    }
+
+    // Controlando o estado do modal de danças
+
+    openModalDancas: boolean = false;
+
+    abrirModalDancas = () => {
+        this.openModalDancas = true;
+    }
+
+    fecharModalDancas = () => {
+        this.openModalDancas = false;
     }
 }
